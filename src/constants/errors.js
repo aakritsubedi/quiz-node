1f6774e449f5fc02f6d9c2405be7a14cada31392
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-05-10 21:21:08 +0545

    Hotfix: Remove repeated constant

---
+++ src/constants/errors.js
@@ -0,0 +1,36 @@
+const NOT_FOUND = {
+  code: 404,
+  message: "No record found in database",
+};
+const AUTH_FAILED = {
+  code: 409,
+  message: "Authentication failed",
+};
+const BAD_REQUEST = {
+  code: 400,
+  message: "Bad request",
+};
+const UNAUTHORIZED = {
+  code: 401,
+  message: "Unauthorized",
+};
+const FORBIDDEN = {
+  code: 403,
+  message: "Forbidden",
+};
+const CONFLICT = {
+  code: 409,
+  message: "Conflict",
+};
+const INTERNAL_SERVER_ERROR = {
+  code: 500,
+  message: "Internal Server Error",
+};
+const SERVICE_UNAVAILABLE = {
+  code: 503,
+  message: "Service Unavailable",
+};
+const LOGIN_TIME_OUT = {
+  code: 440,
+  message: "Login Time-out",
+};
