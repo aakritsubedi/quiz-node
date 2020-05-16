d715106799c8ad62bb76b42f694f82680278c56a
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-05-16 11:47:11 +0545

    Model: fetchAll Que-Ans

--- src/components/que/que.model.js
+++ src/components/que/que.model.js
@@ -3,7 +3,15 @@ const pool = require("../../utils/db/dbConnect");
 
 pool.query = util.promisify(pool.query);
 
-module.exports= {
- 
+module.exports = {
+  fetchAll: async () => {
+    const sql = `SELECT questions.*, options.options FROM questions INNER JOIN options ON questions.id = options.que_id returning *`;
 
-}
+    try {
+      const questions = await pool.query(sql);
+      return questions.rows;
+    } catch (err) {
+      throw err;
+    }
+  },
+};
--- src/components/que/que.service.js
+++ src/components/que/que.service.js
@@ -1,45 +1,52 @@
 const qaModel = require("./que.model");
 
 module.exports = {
-
-    fetchByCategory = async (category) => {
-        let qas = null;
-        try {
-            qas = await qaModel.fetchByCategory(category);
-        } catch (e) {
-            throw e;
-        }
-        return qas;
-    },
-
-    update = async (id) => {
-        let res = null;
-        try {
-            res = await qaModel.update(id);
-        } catch (e) {
-            throw e;
-        }
-        return res;
-    },
-
-    insert = async (data) => {
-        let res = null;
-        try {
-            res = await qaModel.insert(data);
-        } catch (e) {
-            throw e;
-        }
-        return res;
-    },
-
-    delete: async (id) => {
-        let res = null;
-        try {
-            res = await qaModel.delete(id);
-        } catch (e) {
-            throw e;
-        }
-        return res;
+  fetchAll: async () => {
+    let qas = null;
+    try {
+      qas = await qaModel.fetchAll();
+    } catch (e) {
+      throw e;
     }
+    return qas;
+  },
+  fetchByCategory: async (category) => {
+    let qas = null;
+    try {
+      qas = await qaModel.fetchByCategory(category);
+    } catch (e) {
+      throw e;
+    }
+    return qas;
+  },
 
-}
+  update: async (id) => {
+    let res = null;
+    try {
+      res = await qaModel.update(id);
+    } catch (e) {
+      throw e;
+    }
+    return res;
+  },
+
+  insert: async (data) => {
+    let res = null;
+    try {
+      res = await qaModel.insert(data);
+    } catch (e) {
+      throw e;
+    }
+    return res;
+  },
+
+  delete: async (id) => {
+    let res = null;
+    try {
+      res = await qaModel.delete(id);
+    } catch (e) {
+      throw e;
+    }
+    return res;
+  },
+};
