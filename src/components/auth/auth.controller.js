c2d76cc06f3fa0429fb258c3ce0014f3be5aaecf
samir <samirsilwal2424@gmail.com>
2020-05-16 17:07:16 +0545

    Add model for api and cleanup

--- src/components/auth/auth.controller.js
+++ src/components/auth/auth.controller.js
@@ -1,18 +1,19 @@
 const userService = require("../users/users.service");
+const { getApi } = require("../../utils/db/model");
 
 module.exports = {
-    login: async (request, response, next) => {
-        const user = request.body;
-        try {
-            const userData = await userService.login(user);
-            if(!userData) {
-                next({
-                    message: "Coudn't login !!!"
-                })
-            }
-            response.json(userData)
-        } catch (e) {
-            next(e);
-        }
+  login: async (request, response, next) => {
+    const user = request.body;
+    try {
+      const userData = await userService.login(user);
+      if (!userData) {
+        next({
+          message: "Coudn't login !!!",
+        });
+      }
+      response.status(200).json(getApi({ data: userData }));
+    } catch (e) {
+      next(e);
     }
-}
+  },
+};
--- src/components/users/users.controller.js
+++ src/components/users/users.controller.js
@@ -1,70 +1,81 @@
 const userServices = require("./users.service");
+const { getApi } = require("../../utils/db/model");
 
 module.exports = {
-    fetch: async (request, response, next) => {
-        let users = null;
-        try {
-            users = await userServices.fetch()
-            response.json(users)
-        } catch (e) {
-            next(e)
-        }
-        return users;
-    },
-    fetchById: async (request, response, next) => {
-        let user = null;
-        const id = request.params.id;
-        try {
-            user = await userServices.fetchById(id);
-            if(user.length !== 0) {
-                response.json(user);
-            }
-            next({
-                message: 'Not found !!!'
-            });
-          } catch (err) {
-            next(err);
-          }
-    },
-    create: async (request, response, next) => {
-        let user = null;
-        const users = request.body;
-        try {
-          user = await userServices.create(users);
-          if(user){
-            response.json({
-              message: 'Added Successfully'
-            });
-          }
-        } catch (err) {
-          next(err);
-        }
-    },
-    update: async (request, response, next) => {
-        let userInfo = null;
-        const userData = request.body;
-      
-        try {
-          userInfo = await userServices.update(userData);
-          if(userInfo){
-            response.json({
-              message: 'Updated Successfully'
-            });
-          }
-        } catch (err) {   
-          next(err);
-        }
-    },
-    remove: async (request, response, next) => {
-        const id = request.params.id;
-        try {
-            await userServices.remove(id)
-            response.json({
-                message: "Deleted Successfully"
-            })
-        } catch (e) {
-            next(e);
-        }
-    },
+  fetch: async (request, response, next) => {
+    let users = null;
+    try {
+      users = await userServices.fetch();
+      response.status(200).json(getApi({ data: users }));
+    } catch (e) {
+      next(e);
+    }
+  },
+  fetchById: async (request, response, next) => {
+    let user = null;
+    const id = request.params.id;
+    try {
+      user = await userServices.fetchById(id);
+      if (user.length !== 0) {
+        response.status(200).json(getApi({ data: user }));
+      }
+      next({
+        message: "Not found !!!",
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+  create: async (request, response, next) => {
+    let user = null;
+    const users = request.body;
+    try {
+      user = await userServices.create(users);
+      if (user) {
+        response.status(200).json(
+          getApi({
+            data: {
+              message: "Added Successfully",
+            },
+          })
+        );
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+  update: async (request, response, next) => {
+    let userInfo = null;
+    const userData = request.body;
 
-}
+    try {
+      userInfo = await userServices.update(userData);
+      if (userInfo) {
+        response.status(200).json(
+          getApi({
+            data: {
+              message: "Updated Successfully",
+            },
+          })
+        );
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+  remove: async (request, response, next) => {
+    const id = request.params.id;
+    try {
+      await userServices.remove(id);
+      response.status(200).json(
+        getApi({
+          data: {
+            message: "Deleted Successfully",
+          },
+        })
+      );
+    } catch (e) {
+      next(e);
+    }
+  },
+};
