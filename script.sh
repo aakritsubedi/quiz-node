ba2dca84bb4fe6444e268b0538a585b3af9758f5
samir <samirsilwal2424@gmail.com>
2020-05-25 12:34:05 +0545

    demo

--- script.sh
+++ script.sh
@@ -9,6 +9,7 @@ printfln() {
   printf "\n$1\n"
 }
 printfln "$TRAVIS_PULL_REQUEST"
+printfln "$TRAVIS_REPO_SLUG"
 
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$TRAVIS_BRANCH"; else echo "$TRAVIS_PULL_REQUEST_BRANCH"; fi)
 
@@ -18,7 +19,7 @@ printfln "${BRANCH}"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     printfln "i am heres"
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    curl -H "Authorization: token $GITHUB_TOKEN" -X POST \
     -d "{\"body\": \"Hello world\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
