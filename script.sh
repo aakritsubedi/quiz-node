02c994acb75b8c9af0f5e59795ef6f1284a20dda
samir <samirsilwal2424@gmail.com>
2020-05-25 12:28:19 +0545

    demo

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ printfln "${BRANCH}"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     printfln "i am heres"
-    curl -H "Authorization: token 99482c97e6cdd4f48855f047fb8c465886ba30a7" -X POST \
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"Hello world\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
