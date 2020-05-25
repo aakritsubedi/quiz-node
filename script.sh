f8ab3281e6ff20803f957200814ba3e356e5efa7
samir <samirsilwal2424@gmail.com>
2020-05-25 12:11:52 +0545

    tests

--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@ set -e
 printfln() {
   printf "\n$1\n"
 }
+printfln "$TRAVIS_PULL_REQUEST"
 
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$TRAVIS_BRANCH"; else echo "$TRAVIS_PULL_REQUEST_BRANCH"; fi)
 
